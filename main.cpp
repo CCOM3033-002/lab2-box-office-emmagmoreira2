de62e14be6925e710427d736dcd2038a7396d44a
emmagmoreira2 <emma.moreira@upr.edu>
2024-09-06 21:52:47 +0000

    changes on the box format and comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //Emma Moreira Irizarry
 //801237349
-//Colaboraciones: Fernando y Kaleb 
+//Colaboraciones: Fernando Berrios y Kaleb Martinez
 
 
 
@@ -12,10 +12,12 @@ using namespace std;
 
 int main()
 {
-    // Establecer que hace el programa
+// Establish what the program does
     cout << "This program calculates the Gross and Net Box Office Profit and the Distributer's Profit of a movie playing in the theater.\n" ;
 
-    // Declarar variables
+    cout<< "";
+
+// Declare all variables
     string movie_name;
     double adult_tickets;
     double child_tickets;
@@ -25,7 +27,7 @@ int main()
     double Net;
     double Distributor;
 
-// Inicializar variables
+// Initialize the variables
     cout<< "Enter the name of the movie: ";
     getline(cin, movie_name);
 
@@ -35,26 +37,33 @@ int main()
     cout<< "Enter the amount of child tickets sold: ";
     cin>> child_tickets;
 
-// Calcular los precios de los tickets
+    cout<< "";
+
+// Calcualte the number of tickets times the price
     price_adult= 10 * adult_tickets;
     price_child= 6 * child_tickets;
 
-// 
+// Finding the gross office profit that is all the tickets summed
     Gross= price_adult + price_child;
 
+// Finding the net office profit, that is the 20% of the Gross
     Net= Gross * 20 / 100;
 
+// Finding the amount paid to the distributors, gross minus the net
     Distributor= Gross - Net;
 
-    cout<< "------------------\n";
-    cout<< setprecision (6);
+// line of separation
 
+    cout<< "------------------\n";
+    
+
+// Displaying the results 
     cout<< "Movie Name: " << setw(18) << '\"' << movie_name << '\"'<< endl;
     cout<< "Adult Tickets Sold: " << setw(15) << price_adult << endl;
     cout<< "Child Tickets Sold: " << setw(15) << price_child << endl;
-    cout<< "Gross Box Office Profit: "<< setw(8) << setprecision(2) << fixed<< "$ " <<Gross<< endl;
-    cout<< "Net Box Office Profit: " << setw(10) << setprecision(2) << fixed<< "$ " << Net<< endl;
-    cout<< "Amount Paid to Distributor: " << setw(5)<< setprecision(2) << fixed<< "$ " << Distributor<< endl;
+    cout<< "Gross Box Office Profit:    $"<< setw(9) << setprecision(2) << fixed <<Gross<< endl;
+    cout<< "Net Box Office Profit:      $" << setw(9) << setprecision(2) << fixed<< Net<< endl;
+    cout<< "Amount Paid to Distributor: $" << setw(9)<< setprecision(2) << fixed<< Distributor<< endl;
 
     return 0;
     
